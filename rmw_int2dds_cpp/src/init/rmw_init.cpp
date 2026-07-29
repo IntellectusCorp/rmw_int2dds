d982953a197e0c65405c022e00a30ded47eb7ff6
Intellectus-GicheolJeon <gc.jeon@int2.us>
2026-07-29 19:23:12 +0900

    STYLE: Apply ament_uncrustify formatting

--- rmw_int2dds_cpp/src/init/rmw_init.cpp
+++ rmw_int2dds_cpp/src/init/rmw_init.cpp
@@ -107,7 +107,9 @@ acquire_context_resources(ContextData * context_data, const char * enclave)
     return RMW_RET_ERROR;
   }
 
-  ret = int2dds_create_publisher(context_data->participant, nullptr, &context_data->default_publisher);
+  ret = int2dds_create_publisher(
+    context_data->participant, nullptr,
+    &context_data->default_publisher);
   if (ret != INT2DDS_RET_OK) {
     context_data->default_publisher = nullptr;
     release_context_resources(context_data);
@@ -115,7 +117,9 @@ acquire_context_resources(ContextData * context_data, const char * enclave)
     return RMW_RET_ERROR;
   }
 
-  ret = int2dds_create_subscriber(context_data->participant, nullptr, &context_data->default_subscriber);
+  ret = int2dds_create_subscriber(
+    context_data->participant, nullptr,
+    &context_data->default_subscriber);
   if (ret != INT2DDS_RET_OK) {
     context_data->default_subscriber = nullptr;
     release_context_resources(context_data);
